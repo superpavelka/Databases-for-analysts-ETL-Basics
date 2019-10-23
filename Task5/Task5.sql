89318d5128f1e4eaa3b62395e9266efde88ddb13
krilatiy <pos@list.ru>
2019-10-23 22:58:44 +0300

    les5: sql script table for first group

--- Task5/Task5.sql
+++ Task5/Task5.sql
@@ -53,18 +53,28 @@ SELECT user_id, AVG(price) s_price,
             HAVING COUNT(id_o) >= 3 AND MAX(date(o_date)) > date('2017-04-01')
           ) t
 
-SELECT COUNT(user_id)
-    FROM (
-          SELECT user_id, price, o_date, id_o
+SELECT user_id,  
+       CEIL( (30 * s) / days ) s_per_month       
+    FROM
+    (
+SELECT user_id, CEIL(AVG(price)) a, SUM(price) s, TIMESTAMPDIFF(DAY, MIN(o_date), MAX(o_date)) days, COUNT(id_o) c,MAX(o_date) max_date
            FROM orders.orders_before_0517
             GROUP BY user_id
-            HAVING COUNT(id_o) >= 3 AND MAX(date(o_date)) > date('2017-04-01')
-          ) t
+            HAVING c >= 3 AND max_date > date('2017-04-01') AND days > 5
+            ORDER BY days 
+  ) t
+GROUP BY user_id
+ORDER BY s_per_month DESC
 
-  SELECT user_id, TIMESTAMPDIFF(DAY, MIN(o_date), MAX(o_date))
-    FROM (
-          SELECT user_id, price, o_date, id_o
+-- табличка для первой группы
+SELECT user_id, CEIL(AVG(price)) a, 
+                SUM(price) s, 
+                TIMESTAMPDIFF(DAY, MIN(o_date), MAX(o_date)) days, 
+                30/TIMESTAMPDIFF(DAY, MIN(o_date),MAX(o_date))/(COUNT(id_o)-1) a_d, 
+                COUNT(id_o) c,
+                MAX(o_date) max_date,
+                CEIL(30/TIMESTAMPDIFF(DAY, MIN(o_date),MAX(o_date))/(COUNT(id_o)-1)* AVG(price)) sum_per_month
            FROM orders.orders_before_0517
             GROUP BY user_id
-            HAVING COUNT(id_o) >= 3 AND MAX(date(o_date)) > date('2017-04-01')
-          ) t
+            HAVING c >= 3 AND max_date > date('2017-04-01') AND days > 5
+            ORDER BY sum_per_month DESC
