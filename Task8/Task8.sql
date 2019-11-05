07bf3131436c5a41f053a1156d22da51c4ebac1f
krilatiy <pos@list.ru>
2019-11-05 22:37:42 +0300

    les8: 8.3,8.4

--- Task8/Task8.sql
+++ Task8/Task8.sql
@@ -149,87 +149,47 @@ SELECT u1.user_id,u1.user_group gr1,u2.user_group gr2
   JOIN user_groups_010317 u2
   ON u1.user_id = u2.user_id;
 
--- все пользователи Vip
-SELECT user_id
-  FROM
-(
-SELECT user_id,r,f,m,s,
-  CASE WHEN (r = 3 AND f =3 AND m = 3) OR (r = 2 AND f =3 AND m = 3) THEN "Vip"
-       WHEN r = 1 THEN "Lost"
-  ELSE "Regular" END AS user_group
-  FROM
-(
-SELECT user_id,SUM(price) s,
-  CASE WHEN TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) > 60 THEN "1"
-       WHEN TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) <= 60 AND TIMESTAMPDIFF(DAY,o_date,date('2017-12-31')) > 30 THEN "2"
-  ELSE "3" END AS r,
-  CASE WHEN COUNT(*) <= 1 THEN "1"
-       WHEN COUNT(*) > 1 AND COUNT(*) <= 4 THEN "2"
-  ELSE "3" END AS f,
-  CASE WHEN SUM(price) < 5000 THEN "1"
-       WHEN SUM(price) >= 5000 AND SUM(price) < 15000 THEN "2"
-  ELSE "3" END AS m
-  FROM orders.orders_
-  GROUP BY user_id
-) t_rfm
-GROUP BY user_id) t_groups
-  WHERE user_group = "Vip"
+SELECT COUNT(user_id)
+  FROM orders.u02to03
+  WHERE gr1 = 'New' AND gr2 = 'New'
 
--- все пользователи Lost
-SELECT COUNT(user_id), SUM(s)
-  FROM
-(
-SELECT user_id,r,f,m,s,
-  CASE WHEN (r = 3 AND f =3 AND m = 3) OR (r = 2 AND f =3 AND m = 3) THEN "Vip"
-       WHEN r = 1 THEN "Lost"
-  ELSE "Regular" END AS user_group
-  FROM
-(
-SELECT user_id,SUM(price) s,
-  CASE WHEN TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) > 60 THEN "1"
-       WHEN TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) <= 60 AND TIMESTAMPDIFF(DAY,o_date,date('2017-12-31')) > 30 THEN "2"
-  ELSE "3" END AS r,
-  CASE WHEN COUNT(*) <= 1 THEN "1"
-       WHEN COUNT(*) > 1 AND COUNT(*) <= 4 THEN "2"
-  ELSE "3" END AS f,
-  CASE WHEN SUM(price) < 5000 THEN "1"
-       WHEN SUM(price) >= 5000 AND SUM(price) < 15000 THEN "2"
-  ELSE "3" END AS m
-  FROM orders.orders_
-  GROUP BY user_id
-) t_rfm
-GROUP BY user_id) t_groups
-  WHERE user_group = "Lost"
+SELECT COUNT(user_id)
+  FROM orders.u02to03
+  WHERE gr1 = 'New' AND gr2 = 'Vip'
 
--- все пользователи Regular
-SELECT COUNT(user_id), SUM(s)
-  FROM
-(
-SELECT user_id,r,f,m,s,
-  CASE WHEN (r = 3 AND f =3 AND m = 3) OR (r = 2 AND f =3 AND m = 3) THEN "Vip"
-       WHEN r = 1 THEN "Lost"
-  ELSE "Regular" END AS user_group
-  FROM
-(
-SELECT user_id,SUM(price) s,
-  CASE WHEN TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) > 60 THEN "1"
-       WHEN TIMESTAMPDIFF(DAY,MAX(o_date),date('2017-12-31')) <= 60 AND TIMESTAMPDIFF(DAY,o_date,date('2017-12-31')) > 30 THEN "2"
-  ELSE "3" END AS r,
-  CASE WHEN COUNT(*) <= 1 THEN "1"
-       WHEN COUNT(*) > 1 AND COUNT(*) <= 4 THEN "2"
-  ELSE "3" END AS f,
-  CASE WHEN SUM(price) < 5000 THEN "1"
-       WHEN SUM(price) >= 5000 AND SUM(price) < 15000 THEN "2"
-  ELSE "3" END AS m
-  FROM orders.orders_
-  GROUP BY user_id
-) t_rfm
-GROUP BY user_id) t_groups
-  WHERE user_group = "Regular"
+SELECT COUNT(user_id)
+  FROM orders.u02to03
+  WHERE gr1 = 'New' AND gr2 = 'New'
 
--- все пользователи
-SELECT COUNT(DISTINCT user_id)
-  FROM orders.orders_
--- вся прибыль
-SELECT SUM(price)
- FROM orders.orders_
+SELECT COUNT(user_id)
+  FROM orders.u02to03
+  WHERE gr1 = 'Lost' AND gr2 = 'Lost'
+
+SELECT COUNT(user_id)
+  FROM orders.u02to03
+  WHERE gr1 = 'Regular' AND gr2 = 'Vip'
+
+SELECT COUNT(user_id)
+  FROM orders.u01to02
+  WHERE gr1 = 'Regular' AND gr2 = 'Lost'
+
+SELECT COUNT(user_id)
+  FROM orders.u01to02
+  WHERE gr1 = 'Vip' AND gr2 = 'Vip'
+
+SELECT COUNT(user_id)
+  FROM orders.u01to02
+  WHERE gr1 = 'Vip' AND gr2 = 'Lost'
+
+
+SELECT COUNT(user_id)
+  FROM orders.u01to02
+  WHERE gr1 = 'Lost' AND gr2 = 'Regular'
+
+SELECT COUNT(user_id)
+  FROM orders.u01to02
+  WHERE gr1 = 'Lost' AND gr2 = 'Vip'
+
+SELECT COUNT(user_id)
+  FROM orders.u01to02
+  WHERE gr1 = 'Lost' AND gr2 = 'Lost'
